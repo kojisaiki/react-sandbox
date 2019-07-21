015a11818601c52b43fe04bd027dbf12a1cb5d69
Koji Saiki <saikou9901@gmail.com>
2019-07-21 22:48:06 +0900

    class react component

--- src/App.js
+++ src/App.js
@@ -20,12 +20,46 @@ function App() {
         </a>
       </header>
       <MyComponent />
+      <Clock />
     </div>
   );
 }
 
 function MyComponent() {
   return <p>My Component!</p>;
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
