1665264d7d2c7e9fef2d804eb3c1c6df3eeda94f
Koji Saiki <saikou9901@gmail.com>
2019-07-21 22:51:41 +0900

    separate react component to different js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Clock from "./components/Clock";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -27,39 +29,6 @@ function App() {
 
 function MyComponent() {
   return <p>My Component!</p>;
-}
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
 }
 
 export default App;
